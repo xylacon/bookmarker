e8a2fbcf00596b84ff1ec9073535328ac29f7c4c
xylacon <65502587+xylacon@users.noreply.github.com>
2021-08-14 20:27:52 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-var projects = document.getElementById('projects');
-var skills = document.getElementById('skills');
-var education = document.getElementById('education');
-var workExp = document.getElementById('work-exp');
+let projects = document.getElementById('projects');
+let skills = document.getElementById('skills');
+let education = document.getElementById('education');
+let workExp = document.getElementById('work-exp');
 
 projects.childNodes[1].addEventListener('click', changeDisplay);
 skills.childNodes[1].addEventListener('click', changeDisplay);
@@ -10,8 +10,9 @@ workExp.childNodes[1].addEventListener('click', changeDisplay);
 
 function changeDisplay(e)
 {
-	var content = e.target.nextElementSibling;
-	var allContent = document.getElementsByClassName('content');
+	let content = e.target.nextElementSibling;
+	let allContent = document.getElementsByClassName('content');
+	let allParent = document.getElementsByTagName('section');
 
 	if (content.style.display === '' || content.style.display === 'none')
 	{
@@ -19,22 +20,52 @@ function changeDisplay(e)
 		{
 			allContent[i].style.display = 'none';
 		}
+		for (i = 0; i < allParent.length; i++)
+		{
+			allParent[i].style.boxShadow = 'none';
+		}
 		content.style.display = 'block';
+		content.parentNode.style.boxShadow = 'inset 0 0 5px black';
 	}
 	else
 	{
 		content.style.display = 'none';
+		content.parentNode.style.boxShadow = 'none';
 	}
 }
 
-function changeFont()
+// function changeFont()
+// {
+// 	if (document.body.style.fontFamily === '' || document.body.style.fontFamily === 'Ubuntu')
+// 	{
+// 		document.body.style.fontFamily = 'Quicksand';
+// 	}
+// 	else
+// 	{
+// 		document.body.style.fontFamily = 'Ubuntu';
+// 	}
+// }
+
+let x = 0;
+function changeColor()
 {
-	if (document.body.style.fontFamily === '' || document.body.style.fontFamily === 'Ubuntu')
+	themeTransition();
+	let numThemes = ['yellow', 'blue', 'red', 'multi'];
+
+	if ((x + 1) >= numThemes.length)
 	{
-		document.body.style.fontFamily = 'Quicksand';
+		x = 0;
 	}
 	else
 	{
-		document.body.style.fontFamily = 'Ubuntu';
+		x++;
 	}
+
+	document.documentElement.setAttribute('data-theme', numThemes[x]);
+}
+
+function themeTransition()
+{
+    document.documentElement.classList.add("transition");
+    window.setTimeout(() => document.documentElement.classList.remove("transition"), 1000);
 }
