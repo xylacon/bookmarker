1d835d6d478e7bd57e0d486dd6581b6eac4a2c1b
Richard Denton <xylacon@gmail.com>
2022-02-17 19:35:28 -0600

    Cleared everything to make way for new site

--- Sliding Form/main.js
+++
@@ -1,6 +0,0 @@
-const signUpButton = document.getElementById('sign-up');
-const signInButton = document.getElementById('sign-in');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => container.classList.add('right-panel-active'))
-signInButton.addEventListener('click', () => container.classList.remove('right-panel-active'))
