c11655709a2f3a26a9a50f582cbf06a766774b0a
xylacon <65502587+xylacon@users.noreply.github.com>
2022-06-01 16:06:57 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+function changeSize() {
+	const vh = window.innerHeight
+	document.documentElement.style.setProperty("--vh", `${vh}px`)
+}
+
 function changeTheme() {
 	let dark = window.matchMedia("(prefers-color-scheme: dark)").matches
 
@@ -14,5 +19,8 @@ function sendEmail() {
 	window.open("mailto:xylacon@gmail.com")
 }
 
+changeSize()
 changeTheme()
+
+window.addEventListener("resize", changeSize)
 window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", changeTheme)
